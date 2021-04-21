6f7a7a506ad81baefe52729f66d68191483c82c8
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2021-04-21 20:26:33 +0530

    kk

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ export const API = "https://blogwebsiteseo.herokuapp.com/api";
 
 export const APP_NAME = "Blog";
 
-export const DOMAIN = "http://localhost:3000/";
+export const DOMAIN = "https://romantic-leavitt-a70e61.netlify.app/";
 
 // export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
 // export const DISQUS_SHORTNAME = publicRuntimeConfig.DISQUS_SHORTNAME;
