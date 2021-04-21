a8c0eb80ebc79f4ef372c53d4ee347f43de7ce99
kaustavkarmakar2 <kaustavkarmakar2@gmail.com>
2021-04-21 20:20:25 +0530

    kk

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-import getConfig from "next/config";
-const { publicRuntimeConfig } = getConfig();
+// import getConfig from "next/config";
+// const { publicRuntimeConfig } = getConfig();
 
 export const API = "https://blogwebsiteseo.herokuapp.com/api";
 
